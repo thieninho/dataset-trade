8f655b1d7ab5c30956a284d8b3dcc829d8a9f3a6
Cao Đinh Sỹ Vỹ <caodinhsyvy@Vys-Mac.local>
2023-12-29 11:49:55 +0000

    build | 29/12/23

--- src/global/globalVar.js
+++ src/global/globalVar.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "http://14.225.207.19:8086/"
-export const originUrl = "https://chatbot-service.ddns.net/"
+export const originUrl = "http://data-everywhere.myftp.org/"
 export const BASE_URL_CHATBOT = "http://14.225.207.19:8086/"
